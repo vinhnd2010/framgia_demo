9955fc89e655172dd62ae25458bcabb4950a2240
Vinh Nguyen <nguyenducvinh92@gmail.com>
2015-07-16 16:53:00 +0700

    add home, help, about page

--- framgia_demo/app/helpers/application_helper.rb
+++ framgia_demo/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title='')
+		base_title = "Nguyen Duc Vinh's Demo App"
+		if page_title.empty?
+			base_title
+		else
+			pase_title+" | "+base_title
+		end
+	end
 end
